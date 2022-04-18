6db8b0fa86a2456a29a2336c0de21cd1640f87e3
samytichadou <samytichadou@gmail.com>
2022-04-18 12:10:56 +0200

    gui improvement

--- gui.py
+++ gui.py
@@ -19,24 +19,38 @@ class DUPERENDER_PT_main_panel(bpy.types.Panel):
         scn = context.scene
         layout = self.layout
 
-        if not scn.duperender_use_duperender:
+        # if not scn.duperender_use_duperender:
+        #     layout.enabled = False
+        if not scn.duperender_render:
             layout.enabled = False
 
-        if context.scene.render.is_movie_format:
-            layout.label(text="Movie Format Output used", icon = "INFO")
-        elif scn.render.use_overwrite:
-            layout.label(text="Overwrite Render used", icon = "INFO")
-        elif scn.duperender_dupelist == "":
-            layout.label(text="No dupe frames", icon = "INFO")
+        bigcol = layout.column(align=True)
 
-        box = layout.box()
+        # render infos box
+        box = bigcol.box()
+        col = box.column(align=True)
+        if scn.render.is_movie_format:
+            col.label(text="Movie Format Output used", icon = "INFO")
+        elif scn.render.use_overwrite:
+            col.label(text="Overwrite Render used", icon = "INFO")
+        elif scn.duperender_dupelist == "":
+            col.label(text="No dupe frames", icon = "INFO")
+        else:
+            col.label(text="All set", icon = "CHECKMARK")
+        col.separator()
+        col.prop(scn.render.image_settings, "file_format")
+        col.prop(scn.render, "use_overwrite")
+
+        # process box
+        box = bigcol.box()
         col = box.column(align=True)
         col.operator("duperender.find_dupe_frames")
 
         col.prop(scn, "duperender_dupelist", text="Dupes")
         col.prop(scn, "duperender_originallist", text="Originals")
 
-        box = layout.box()
+        # options box
+        box = bigcol.box()
         col = box.column(align=True)
         col.operator("duperender.hash_frame")
 
