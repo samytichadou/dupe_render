523b2fbacff0ae5f804df8073e75905c6d8a2ca4
samytichadou <samytichadou@gmail.com>
2022-06-05 15:04:18 +0200

    remove placeholder op options

--- remove_placeholders_operator.py
+++ remove_placeholders_operator.py
@@ -2,27 +2,39 @@ import bpy
 import os
 
 
-def remove_placeholders(scene):
-    dupe_list = scene.duperender_dupelist.split(",")
+def remove_single_placeholder(scene, frame_number):
+    path = scene.render.frame_path(frame=frame_number)
+    if os.path.isfile(path):
+        if os.path.getsize(path)!=0:
+            print("Dupe Render --- frame %i is not a placeholder, skipping" % frame_number)
+            return False
+        try:
+            os.remove(path)
+            print("Dupe Render --- frame %i removed" % frame_number)
+            return True
+        except OSError as error:
+            print(error)
+            print("Dupe Render --- unable to remove frame %i, skipping" % frame_number)
+            return False
+    else:
+        print("Dupe Render --- frame %i does not exist" % frame_number)
+        return False
 
+def remove_dupe_placeholders(scene, scene_range):
+    dupe_list = scene.duperender_dupelist.split(",")
     for dupe in dupe_list:
         i = int(dupe)
-        path = scene.render.frame_path(frame=i)
-        #remove dupe frame
-        if os.path.isfile(path):
-            #check if placeholder
-            if os.path.getsize(path)!=0:
-                print("Dupe Render --- frame %i is not a placeholder, skipping" % i)
-                continue
-            try:
-                os.remove(path)
-                print("Dupe Render --- frame %i removed" % i)
-            except OSError as error:
-                print(error)
-                print("Dupe Render --- unable to remove frame %i, skipping" % i)
-                continue
-        else:
-            print("Dupe Render --- frame %i does not exist" % i)
+        if not scene_range \
+        or i in range(scene.frame_start, scene.frame_end+1):
+            remove_single_placeholder(scene, i)
+
+def remove_placeholders(scene, scene_range):
+    if scene_range:
+        frame_range=range(scene.frame_start, scene.frame_end+1)
+    else:
+        frame_range=range(scene.duperender_frame_start, scene.duperender_frame_end+1)
+    for i in frame_range:
+        remove_single_placeholder(scene, i)
 
 
 class DUPERENDER_OT_remove_placeholders(bpy.types.Operator):
@@ -31,23 +43,41 @@ class DUPERENDER_OT_remove_placeholders(bpy.types.Operator):
     bl_description = "Remove Dupe frames placeholders previously created"
     #bl_options = {'INTERNAL'}
 
+    scene_range : bpy.props.BoolProperty(
+        name = "Scene Frame Range",
+        description="Remove placeholders in the scene frame range",
+        )
+
+    all_frames : bpy.props.BoolProperty(
+        name = "All frames",
+        description="Remove all frames, not only dupe frames",
+        )
+
     @classmethod
     def poll(cls, context):
-        return context.scene.duperender_dupelist!=""
+        return True
 
     def invoke(self, context, event):
         return context.window_manager.invoke_props_dialog(self)
  
     def draw(self, context):
         layout = self.layout
-        layout.label(text="Proceed with caution", icon="ERROR")
-        col=layout.column(align=True)
+        row=layout.row()
+        row.prop(self, "scene_range")
+        row.prop(self, "all_frames")
+
+        box=layout.box()
+        col=box.column(align=True)
+        col.label(text="Proceed with caution", icon="ERROR")
         col.label(text="This action will remove existing dupe placeholders")
         col.label(text="Are you sure ?")  
 
     def execute(self, context):
         scn = context.scene
-        remove_placeholders(scn)
+        if self.all_frames:
+            remove_placeholders(scn, self.scene_range)
+        else:
+            remove_dupe_placeholders(scn, self.scene_range)
 
         self.report({'INFO'}, "Placeholders removed")
         
